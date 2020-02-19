ff43b85d18e658e0aba493a5fbdbf6e6e893b8c7
jelleimu <jelle@imu.nl>
2020-02-19 11:49:36 +0100

    prepared demo

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,57 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="app">
+        <p>Even though this input is modeled to user.name, the name won't be updated</p>
+        <p>This happens because there the getters and setters for 'user' aren't being send by the ORM</p>
+        <p>This behavior changed in version 0.34.0, prior the getters and setters were being send</p>
+        <input type="text" v-model="user.name" v-if="user">
     </div>
-    <router-view/>
-  </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+    import User from './models/User.js'
 
-#nav {
-  padding: 30px;
+    export default {
+        computed: {
+            user() {
+                return User.find(1)
+            }
+        },
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+        mounted() {
+            User.insert(
+                {
+                    data: {
+                        id: 1,
+                        name: 'John',
+                        email: 'john@doe.com',
+                    }
+                }
+            )
 
-    &.router-link-exact-active {
-      color: #42b983;
+            console.log(this.user)
+        }
     }
-  }
-}
+</script>
+
+<style lang="scss">
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+    }
+
+    #nav {
+        padding: 30px;
+
+        a {
+            font-weight: bold;
+            color: #2c3e50;
+
+            &.router-link-exact-active {
+                color: #42b983;
+            }
+        }
+    }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import VuexORM from '@vuex-orm/core'
+import User from '../models/User.js'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+// Create a new database instance.
+const database = new VuexORM.Database()
+
+// Register Models to the database.
+database.register(User)
+
+// Create Vuex Store and register database through Vuex ORM.
+const store = new Vuex.Store({
+    plugins: [VuexORM.install(database)]
 })
+
+export default store
